7934c2e2c2c6fae3963b6f9fc6ce385c5ec82df1
Ivory <IvoryEC@gmail.com>
2020-11-17 23:22:49 -0500

    bring in Alicias commit

--- BioLockJ/BioLockJ_R/RScripts_AAS/pco.R
+++ BioLockJ/BioLockJ_R/RScripts_AAS/pco.R
@@ -2,21 +2,31 @@
 #Date: 08-17-2020
 #Description: Ordination plot.
 
-rm(list=ls())
+#BioLockJ configuration: Ali Sorgen
+#Date: 09-30-2020
+# rm(list=ls())
 
 #Libraries
 library(vegan)
-
+library(stringr)
 set.seed(123)
 
-output<-"./output/"
+pipeRoot = dirname(dirname(getwd()))
+moduleDir <- dirname(getwd())
+input <- paste0(pipeRoot,"/",str_subset(dir(pipeRoot), "CombineCountTableslog10"),"/output/")
+output = file.path(dirname(getwd()),"output/")
+# output<-"./output/"
 taxa<-"Genus"
 colors=c("red","blue","darkgreen","darkorange2","purple","hotpink","black","firebrick")
-source("./Rcode/RYGB_IntegratedAnalysis/functions.R")
+# source("./Rcode/RYGB_IntegratedAnalysis/functions.R")
+funcScript <- paste0(moduleDir,"/resources/functions.R")
+source(funcScript)
 
 
-myT<-read.table(paste0(output,taxa,"_countTable_merged_log10.txt"),sep="\t",header = TRUE)
-meta<-read.table(paste0(output,"metaData_merged.txt"),sep="\t",header = TRUE)
+# myT<-read.table(paste0(output,taxa,"_countTable_merged_log10.txt"),sep="\t",header = TRUE)
+myT<-read.table(paste0(input,taxa,"_countTable_merged_log10.txt"),sep="\t",header = TRUE)
+# meta<-read.table(paste0(output,"metaData_merged.txt"),sep="\t",header = TRUE)
+meta<-read.table(paste0(input,"metaData_merged.txt"),sep="\t",header = TRUE)
 meta$TimeEachStudy<-paste0(meta$Study,"_",meta$time)
 
 pdf(paste0(output,taxa,"_PCO.pdf"),width = 5,height = 10)
@@ -28,29 +38,30 @@ getPCO(myT,meta,"TimeEachStudy",names=levels(factor(meta$TimeEachStudy)),colors)
 dev.off()
 
 #PERMANOVA 
-adonis(myT~factor(meta$time)*factor(meta$Study))
-"Call:
-adonis(formula = myT ~ factor(meta$time) * factor(meta$Study)) 
-
-Permutation: free
-Number of permutations: 999
-
-Terms added sequentially (first to last)
-
-                                      Df SumsOfSqs MeanSqs F.Model
-factor(meta$time)                      1    0.7379 0.73786  9.1916
-factor(meta$Study)                     3    6.7607 2.25357 28.0729
-factor(meta$time):factor(meta$Study)   3    0.2781 0.09269  1.1546
-Residuals                            180   14.4496 0.08028        
-Total                                187   22.2262                
-                                          R2 Pr(>F)    
-factor(meta$time)                    0.03320  0.001 ***
-factor(meta$Study)                   0.30418  0.001 ***
-factor(meta$time):factor(meta$Study) 0.01251  0.219    
-Residuals                            0.65012           
-Total                                1.00000           
----
-Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1"
+permanova <- adonis(myT~factor(meta$time)*factor(meta$Study))
+capture.output(permanova, file = paste0(output, taxa, "_permanovaResults.txt"))
+# "Call:
+# adonis(formula = myT ~ factor(meta$time) * factor(meta$Study)) 
+# 
+# Permutation: free
+# Number of permutations: 999
+# 
+# Terms added sequentially (first to last)
+# 
+#                                       Df SumsOfSqs MeanSqs F.Model
+# factor(meta$time)                      1    0.7379 0.73786  9.1916
+# factor(meta$Study)                     3    6.7607 2.25357 28.0729
+# factor(meta$time):factor(meta$Study)   3    0.2781 0.09269  1.1546
+# Residuals                            180   14.4496 0.08028        
+# Total                                187   22.2262                
+#                                           R2 Pr(>F)    
+# factor(meta$time)                    0.03320  0.001 ***
+# factor(meta$Study)                   0.30418  0.001 ***
+# factor(meta$time):factor(meta$Study) 0.01251  0.219    
+# Residuals                            0.65012           
+# Total                                1.00000           
+# ---
+# Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1"
 
 
 
